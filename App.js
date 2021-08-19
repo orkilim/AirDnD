1cdb344b90f27654d2df78562657cebc65099ba9
or kilim <48524233+orkilim@users.noreply.github.com>
2021-08-19 15:28:13 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,41 @@
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App  = () => {
+  
+  return (
+    
+      
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Text>hello world</Text>
+        </View>
+      </ScrollView>
+    
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
